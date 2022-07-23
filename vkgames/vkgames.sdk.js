b5d7c68ed6a6a61480045ef40581cd212683bba1
AleksandrZheleznyi <96437481+AleksandrZheleznyi@users.noreply.github.com>
2022-07-23 22:19:46 +0600

    up

--- vkgames/vkgames.sdk.js
+++ vkgames/vkgames.sdk.js
@@ -151,7 +151,15 @@ async function BridgeAppAddToFavorites(request)
 
 function GetLanguageCode()
 {
+    if(enabledDefaultLanguage == true)
+        return defaultLanguageCode;
     return navigator.language || navigator.userLanguage;
+}
+
+// Envorinment
+function GetEnvironmentJson()
+{
+    return JSON.stringify(environment);
 }
 
 BridgeInitialize();
